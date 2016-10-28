e04e941d71b587b7f648658ab3542fca787c46a4
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2016-10-29 08:31:17 +0900

    Add validator classes

---
+++ pytablereader/_validator.py
@@ -0,0 +1,66 @@
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <gogogo.vm@gmail.com>
+"""
+
+from __future__ import absolute_import
+import abc
+import os.path
+
+import dataproperty
+import pathvalidate as pv
+
+from ._constant import SourceType
+from .error import InvalidFilePathError
+from .error import InvalidDataError
+
+
+class ValidatorInterface(object):
+
+    @abc.abstractproperty
+    def source_type(self):
+        pass
+
+    @abc.abstractmethod
+    def validate(self):
+        pass
+
+
+class BaseValidator(ValidatorInterface):
+
+    @property
+    def source(self):
+        return self.__source
+
+    def __init__(self, source):
+        self.__source = source
+
+
+class FileValidator(BaseValidator):
+
+    @property
+    def source_type(self):
+        return SourceType.FILE
+
+    def validate(self):
+        try:
+            pv.validate_file_path(self.source)
+        except pv.NullNameError:
+            raise IOError("file path is empty")
+        except (pv.InvalidCharError, pv.InvalidLengthError) as e:
+            raise InvalidFilePathError(e)
+
+        if not os.path.isfile(self.source):
+            raise IOError("file not found")
+
+
+class TextValidator(BaseValidator):
+
+    @property
+    def source_type(self):
+        return SourceType.TEXT
+
+    def validate(self):
+        if dataproperty.is_empty_string(self.source):
+            raise InvalidDataError("data source is empty")
---
+++ test/test_validator.py
@@ -0,0 +1,70 @@
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <gogogo.vm@gmail.com>
+"""
+
+from __future__ import absolute_import
+
+import pytest
+
+import pytablereader as ptr
+from pytablereader._constant import SourceType
+from pytablereader._validator import FileValidator
+from pytablereader._validator import TextValidator
+
+
+class Test_FileValidator_validate:
+
+    @pytest.mark.parametrize(["value"], [
+        ["test"],
+    ])
+    def test_normal(self, tmpdir, value):
+        p_file_path = tmpdir.join(value)
+
+        with open(str(p_file_path), "w") as _f:
+            pass
+
+        validator = FileValidator(str(p_file_path))
+        assert validator.source_type == SourceType.FILE
+        validator.validate()
+
+    @pytest.mark.parametrize(["value", "expected"], [
+        [None, IOError],
+        ["", IOError],
+    ])
+    def test_exception_null(self, value, expected):
+        validator = FileValidator(value)
+
+        with pytest.raises(expected):
+            validator.validate()
+
+    @pytest.mark.parametrize(["value", "expected"], [
+        ["te\0st", ptr.InvalidFilePathError]
+    ])
+    def test_exception_invalid_path(self, tmpdir, value, expected):
+        validator = FileValidator(value)
+
+        with pytest.raises(expected):
+            validator.validate()
+
+
+class Test_TextValidator_validate:
+
+    @pytest.mark.parametrize(["value"], [
+        ["test"],
+    ])
+    def test_normal(self, value):
+        validator = TextValidator(value)
+        assert validator.source_type == SourceType.TEXT
+        validator.validate()
+
+    @pytest.mark.parametrize(["value", "expected"], [
+        [None, ptr.InvalidDataError],
+        ["", ptr.InvalidDataError],
+    ])
+    def test_exception(self, value, expected):
+        validator = TextValidator(value)
+
+        with pytest.raises(expected):
+            validator.validate()
