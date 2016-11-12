1bd9d829d049c3c55079deb6af4f7ab18f248764
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2016-11-13 08:21:15 +0900

    Update docstring

--- pytablereader/loadermanager/_url.py
+++ pytablereader/loadermanager/_url.py
@@ -16,11 +16,23 @@ class TableUrlLoader(TableLoaderManager):
     Loader class to loading tables from URL.
 
     :param str url: URL to load.
-    :param str format_name: Defaults to |None|.
-    :param dict proxies: Defaults to |None|.
+    :param str format_name: Data format name to load.
+        Supported formats are:
+            * ``"csv"``
+            * ``"excel"``
+            * ``"html"``
+            * ``"json"``
+            * ``"markdown"``
+            * ``"mediawiki"``
+    :param dict proxies: http/https proxy information.
 
         .. seealso::
             - `requests proxies <http://requests-docs-ja.readthedocs.io/en/latest/user/advanced/#proxies>`__
+
+    :raises pytablereader.LoaderNotFoundError:
+        If appropriate loader not found to loading the URL.
+    :raises pytablereader.HTTPError:
+        If loader received an HTTP error when access to the URL.
     """
 
     def __init__(self, url, format_name=None, proxies=None):
