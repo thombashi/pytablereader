68f50e83f4b06d3e45bfe1610d50d88e73bde8af
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2016-11-12 19:37:10 +0900

    Fix for python 2 compatibility

--- examples/load_table_from_url.py
+++ examples/load_table_from_url.py
@@ -3,6 +3,8 @@
 
 from __future__ import print_function
 from __future__ import unicode_literals
+import io
+
 import pytablereader
 
 print("\n".join([
@@ -14,7 +16,7 @@ loader = pytablereader.TableUrlLoader(
     "https://en.wikipedia.org/wiki/List_of_unit_testing_frameworks",
     "html")
 
-with open("hoge.rst", "w", encoding="utf-8") as f:
+with io.open("hoge.rst", "w", encoding=loader.encoding) as f:
     for table_data in loader.load():
         print("{:s}".format(table_data.dumps()))
         f.write(table_data.dumps())
