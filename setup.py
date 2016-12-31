401d55a72a924441222f5554f501969bf7054e2d
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2016-12-31 22:31:20 +0900

    Bump version

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ with open(os.path.join(REQUIREMENT_DIR, "test_requirements.txt")) as f:
 
 setuptools.setup(
     name="pytablereader",
-    version="0.8.3",
+    version="0.8.4",
     url="https://github.com/thombashi/pytablereader",
 
     author="Tsuyoshi Hombashi",
