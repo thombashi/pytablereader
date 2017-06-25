4e236d7222e6e847a50791253bc042dbdee3d288
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2017-06-25 16:04:21 +0900

    Modify raising error class.

--- pytablereader/mediawiki/formatter.py
+++ pytablereader/mediawiki/formatter.py
@@ -7,13 +7,17 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
+from ..error import PypandocImportError
 from ..html.formatter import HtmlTableFormatter
 
 
 class MediaWikiTableFormatter(HtmlTableFormatter):
 
     def __init__(self, source_data):
-        import pypandoc
+        try:
+            import pypandoc
+        except ImportError as e:
+            raise PypandocImportError(e)
 
         super(MediaWikiTableFormatter, self).__init__(
             pypandoc.convert_text(source_data, "html", format="mediawiki"))
