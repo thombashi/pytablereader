883d8e832b4ac1e194c1352e610c2f80af3a526e
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2017-08-01 09:29:23 +0900

    Modify logging setup

--- pytablereader/_logger.py
+++ pytablereader/_logger.py
@@ -59,8 +59,8 @@ def set_log_level(log_level):
         set_logger(is_enable=False)
     else:
         set_logger(is_enable=True)
-        logger.level = log_level
 
+    logger.level = log_level
     dataproperty.set_log_level(log_level)
 
     try:
