bf31758238151d786a255e7c641852e1f9fc596c
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2017-08-19 17:23:42 +0900

    Bump version to 0.13.3

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ pytest_runner_require = ["pytest-runner"] if needs_pytest else []
 
 setuptools.setup(
     name=MODULE_NAME,
-    version="0.13.2",
+    version="0.13.3",
     url="https://github.com/thombashi/{}".format(MODULE_NAME),
 
     author="Tsuyoshi Hombashi",
