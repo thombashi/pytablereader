d19dc38042d3bc650fb0c673c8c66227013612d8
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2017-11-13 05:49:05 +0900

    Try to fix Appveyor CI tests

---
+++ invoke_pytest.py
@@ -0,0 +1,17 @@
+# encoding: utf-8
+
+"""
+Unit tests at Windows environments required to invoke from py module,
+because of multiprocessing:
+http://py.readthedocs.io/en/latest/faq.html?highlight=cmdline#issues-with-py-test-multiprocess-and-setuptools
+"""
+
+import multiprocessing
+import sys
+
+import py
+
+
+if __name__ == '__main__':
+    multiprocessing.freeze_support()
+    sys.exit(py.test.cmdline.main())
