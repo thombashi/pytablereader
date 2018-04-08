9847886f060483fb38ea6c00ddaba317fadaea29
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-04-08 21:47:40 +0900

    Apply isort

--- pytablereader/__init__.py
+++ pytablereader/__init__.py
@@ -7,65 +7,22 @@
 from __future__ import absolute_import
 
 from tabledata import (
-    SQLiteTableDataSanitizer,
-    TableData,
-    TableDataSanitizer,
-
-    InvalidTableNameError,
-    InvalidHeaderNameError,
-    InvalidDataError,
-    EmptyDataError,
-)
+    EmptyDataError, InvalidDataError, InvalidHeaderNameError, InvalidTableNameError,
+    SQLiteTableDataSanitizer, TableData, TableDataSanitizer)
 
 from ._constant import PatternMatch
-from ._logger import (
-    logger,
-    set_logger,
-    set_log_level,
-)
-from .csv.core import (
-    CsvTableFileLoader,
-    CsvTableTextLoader,
-)
+from ._logger import logger, set_log_level, set_logger
+from .csv.core import CsvTableFileLoader, CsvTableTextLoader
 from .error import (
-    ValidationError,
-    InvalidPathError,
-    InvalidFilePathError,
-    InvalidUrlError,
-    OpenError,
-    LoaderNotFoundError,
-    HTTPError,
-    ProxyError,
-    PypandocImportError,
-)
-from .html.core import (
-    HtmlTableFileLoader,
-    HtmlTableTextLoader,
-)
-from .json.core import (
-    JsonTableFileLoader,
-    JsonTableTextLoader,
-)
-from .loadermanager import (
-    TableFileLoader,
-    TableUrlLoader,
-)
-from .ltsv.core import (
-    LtsvTableFileLoader,
-    LtsvTableTextLoader,
-)
-from .markdown.core import (
-    MarkdownTableFileLoader,
-    MarkdownTableTextLoader,
-)
-from .mediawiki.core import (
-    MediaWikiTableFileLoader,
-    MediaWikiTableTextLoader,
-)
+    HTTPError, InvalidFilePathError, PathError, InvalidUrlError, LoaderNotFoundError,
+    OpenError, ProxyError, PypandocImportError, ValidationError)
+from .html.core import HtmlTableFileLoader, HtmlTableTextLoader
+from .json.core import JsonTableFileLoader, JsonTableTextLoader
+from .loadermanager import TableFileLoader, TableUrlLoader
+from .ltsv.core import LtsvTableFileLoader, LtsvTableTextLoader
+from .markdown.core import MarkdownTableFileLoader, MarkdownTableTextLoader
+from .mediawiki.core import MediaWikiTableFileLoader, MediaWikiTableTextLoader
 from .spreadsheet.excelloader import ExcelTableFileLoader
 from .spreadsheet.gsloader import GoogleSheetsTableLoader
 from .sqlite.core import SqliteFileLoader
-from .tsv.core import (
-    TsvTableFileLoader,
-    TsvTableTextLoader,
-)
+from .tsv.core import TsvTableFileLoader, TsvTableTextLoader
