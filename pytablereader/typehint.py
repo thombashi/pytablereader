722c318e7933b21efe6c86500530ebb9ab512989
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2020-04-16 01:20:00 +0900

    Add typehint.py

---
+++ pytablereader/typehint.py
@@ -0,0 +1,14 @@
+from typepy.type import (
+    Bool,
+    DateTime,
+    Dictionary,
+    Infinity,
+    Integer,
+    IpAddress,
+    List,
+    Nan,
+    NoneType,
+    NullString,
+    RealNumber,
+    String,
+)
