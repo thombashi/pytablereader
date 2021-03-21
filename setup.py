d92251d4f5db7e5b6cd5516676d1a875998bdac1
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2021-03-21 19:16:13 +0900

    Update requirements

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ excel_requires = ["excelrd>=2.0.2"]
 
 markdown_requires = ["Markdown>=2.6.6,<4"]
 mediawiki_requires = ["pypandoc"]
-sqlite_requires = ["SimpleSQLite>=1.1.1,<2"]
+sqlite_requires = ["SimpleSQLite>=1.1.4,<2"]
 gs_requires = ["gspread", "oauth2client", "pyOpenSSL"] + sqlite_requires
 logging_requires = ["loguru>=0.4.1,<1"]
 url_requires = ["requests>=2.18.4,<3", "retryrequests>=0.0.2,<1"]
